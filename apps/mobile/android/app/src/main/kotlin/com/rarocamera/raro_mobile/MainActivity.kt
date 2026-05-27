e87bbc9652c75a059abcdd86e5a642118c98e437
ZenniTTy <eduardo@ianelli.tech>
2026-05-26 23:16:38 -0300

    feat(bridge): android camera hostapi impl + platformview + factory registration

--- apps/mobile/android/app/src/main/kotlin/com/rarocamera/raro_mobile/MainActivity.kt
+++ apps/mobile/android/app/src/main/kotlin/com/rarocamera/raro_mobile/MainActivity.kt
@@ -1,5 +1,27 @@
 package com.rarocamera.raro_mobile
 
+import com.rarocamera.raro_mobile.camera.CameraHostApiImpl
+import com.rarocamera.raro_mobile.camera.CameraManager
+import com.rarocamera.raro_mobile.camera.CameraPlatformViewFactory
+import com.rarocamera.raro_mobile.generated.camera.CameraFlutterApi
+import com.rarocamera.raro_mobile.generated.camera.CameraHostApi
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    val messenger = flutterEngine.dartExecutor.binaryMessenger
+    val manager = CameraManager(applicationContext, this)
+    val flutterApi = CameraFlutterApi(messenger)
+    val hostApi = CameraHostApiImpl(manager, flutterApi)
+    CameraHostApi.setUp(messenger, hostApi)
+    flutterEngine
+      .platformViewsController
+      .registry
+      .registerViewFactory(
+        "com.rarocamera/camera_preview",
+        CameraPlatformViewFactory(manager),
+      )
+  }
+}
